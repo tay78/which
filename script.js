9d3962b8f43f8959810e16a8e942bcd154a03879
tay78 <tkwilliams-s@cps.edu>
2025-03-13 16:28:33 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+let color = document.querySelector(".color");
+let quiz = document.querySelector(".quiz");
+let activity = document.querySelector(".activity");
+
+quiz.onClick = function(){
+
+activity = activity.value;
+if (activity === "water related"){
+
+}
+else if (activity === "hiking"){
+
+
+}else if(activity === "sports"){
+
+
+}else if(activity === "reading"){
+
+
+
+}else{
+
+
+
+}
+};
