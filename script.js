6e2e1fd74fde272c992cf7c13ce31ad7b534b282
tay78 <tkwilliams-s@cps.edu>
2025-04-02 16:31:14 +0000

    erd

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 let color = document.querySelector(".color");
-let quiz = document.querySelector(".quiz");
+let character = document.querySelector(".character");
 let activity = document.querySelector(".activity");
 let trait = document.querySelector(".trait");
 let buttons = document.querySelector(".buttons");
+let text = document.querySelector('h4');
 let score1 = 0;
 let score2 = 0;
 let score3 = 0;
@@ -61,23 +62,27 @@ trait.style.display ="none";
     
     }
     activity.style.display="block";
-
+    console.log(score1);
+    console.log(score2);
+    console.log(score3);
+    console.log(score4);
+    console.log(score5);
     });
     trait.addEventListener("click", function(){
         //  activity = activity.value;
         color.style.display ="none";
         trait.style.display ="block";
       activity.style.display="none";
-          if (buttons === "answer1"){
+          if (answer === "knowledge"){
           score1=score1+1;
           }
-          else if (buttons === "answer2"){
+          else if (answer === "humor"){
               score2=score2+1;
           
-          }else if(buttons === "answer3"){
+          }else if(answer === "resourcefulness"){
               score3=score3+1;
           
-          }else if(buttons === "answer4"){
+          }else if(answer === "looks"){
               score4=score4+1;
           
           }else{
@@ -90,3 +95,36 @@ console.log(score3);
 console.log(score4);
 console.log(score5);
           });
+
+    character.addEventListener("keydown", function(){
+        color.style.display ="none";
+        trait.style.display ="none";
+      activity.style.display="none";
+        if(event.keyCode == 13){
+if(score1>= score2 && score1>= score3 && score1>= score4 && score1>= score5){
+
+text.innerHTML = "percy";
+}
+if(score2>= score1 && score2>= score3 && score2>= score4 && score2>= score5){
+
+    text.innerHTML = "nico";
+
+
+}if(score3>= score2 && score3>= score1 && score3>= score4 && score3>= score5){
+
+    text.innerHTML = "grover";
+
+
+}if(score4>= score2 && score4>= score3 && score4>= score1 && score4>= score5){
+
+    text.innerHTML = "annabeth";
+
+
+}if(score5>= score2 && score5>= score3 && score5>= score4 && score5>= score1){
+
+    text.innerHTML = "luke";
+
+}
+}
+
+    });
